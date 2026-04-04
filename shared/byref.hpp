d79d2f051eca7a87d5f4c30aa68a64dbbfb6abf3
Metalit <48568911+Metalit@users.noreply.github.com>
2026-04-04 00:22:05 -0600

    Fix accidental copy

--- shared/byref.hpp
+++ shared/byref.hpp
@@ -7,22 +7,22 @@
 template <typename T>
 requires(!std::is_reference_v<T>)
 struct by_ref {
-    constexpr by_ref(T& val) noexcept : ref(val) {}
-    explicit constexpr by_ref(void* val) noexcept : ref(*reinterpret_cast<T*>(val)) {}
+    constexpr by_ref(T& val) noexcept : ref(&val) {}
+    explicit constexpr by_ref(void* val) noexcept : ref(reinterpret_cast<T*>(val)) {}
 
-    constexpr void* convert() const noexcept { return reinterpret_cast<void*>(&ref); }
+    constexpr void* convert() const noexcept { return reinterpret_cast<void*>(ref); }
 
-    constexpr T& operator*() noexcept { return ref; }
-    constexpr T const& operator*() const noexcept { return ref; }
-    constexpr T* operator->() noexcept { return &ref; }
-    constexpr T const* operator->() const noexcept { return &ref; }
+    constexpr T& operator*() noexcept { return *ref; }
+    constexpr T const& operator*() const noexcept { return *ref; }
+    constexpr T* operator->() noexcept { return ref; }
+    constexpr T const* operator->() const noexcept { return ref; }
 
     by_ref<T>& operator=(T& other) {
-        ref = other;
+        ref = &other;
         return *this;
     }
 
-    T& ref;
+    T* ref;
 };
 
 // We do not need a no_arg_class specialization for by_ref, since it will never get to that point.
