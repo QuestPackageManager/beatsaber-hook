5107c170d7dc3eea5cd14a86f49c860e3aa88efb
Metalit <48568911+Metalit@users.noreply.github.com>
2026-04-03 21:25:44 -0600

    Fix valuew

--- shared/valuew.hpp
+++ shared/valuew.hpp
@@ -2,7 +2,7 @@
 
 #include "types.hpp"
 
-template <size_t S>
+template <size_t S, i2c::str_lit Namespace, i2c::str_lit Name>
 struct ValueW {
     constexpr ValueW() = default;
     constexpr ValueW(ValueW const&) = default;
@@ -17,19 +17,19 @@ struct ValueW {
     constexpr ValueW& operator=(ValueW const& o) = default;
 
     static constexpr size_t Size = S;
+    static constexpr i2c::str_lit Ns = Namespace;
+    static constexpr i2c::str_lit Nm = Name;
     std::array<std::byte, S> instance;
 };
 
-template <size_t S>
-struct i2c::type_check::no_arg_class<ValueW<S>> {
-    static inline Il2CppClass* get() {
-        static Il2CppClass* klass = get_class_from_name("System", "ValueType");
-        return klass;
-    }
-};
-// Types inheriting from ValueW will also automatically be marked as value types.
+// Types inheriting from ValueW will also inherit its type markers.
 template <typename T>
-requires(std::derived_from<T, ValueW<T::Size>>)
+requires(std::derived_from<T, ValueW<T::Size, T::Ns, T::Nm>>)
+struct i2c::type_check::no_arg_class<T> {
+    static inline Il2CppClass* get() { return class_of<const_type<T::Ns, T::Nm>>(); }
+};
+template <typename T>
+requires(std::derived_from<T, ValueW<T::Size, T::Ns, T::Nm>>)
 struct i2c::type_markers::value_type_trait<T> {
     static constexpr bool value = true;
 };
